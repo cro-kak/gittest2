d63ee3066f1ebb7e3057d01eb6ea7c66c0d569b3
cro-kak <crocodile.kokak@gmail.com>
2025-06-06 13:33:39 +0800

    Java file created

---
+++ Git.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Git {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       System.out.print("Enter a number:");
+       int num1 = sc.nextInt();
+
+    
+    }
+}
